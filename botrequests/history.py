04ea7f7e3ac962e568e1a96a6d458e0d487bf450
mrSerjo <mrserjo@mail.ru>
2022-02-22 12:36:04 +0300

    error fixed

--- botrequests/history.py
+++ botrequests/history.py
@@ -10,7 +10,7 @@ def history(hotels_data, user_data):
     time = arrow.utcnow().shift(hours=+3).format('YYYY-MM-DD HH:mm:ss')
     result, query_url = hotels_data
     my_list = []
-    for i_hotel, i_data in result.itemes():
+    for i_hotel, i_data in result.items():
         my_list.append("<a href='{url}'>{name}</a>".format(
             name=i_hotel, url='https://hotels.com/ho' + str(i_data['id'])))
     key = "<a href='{query_url}'>{func} {city_name}</a>\n({time})".format(
